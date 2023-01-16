ce8fe5db11d3e3f2ce757083ce08a726b9e7ae22
Anderson Joel <domnquo@gmail.com>
2023-01-16 16:19:09 -0300

    req16

---
+++ src/pages/Ranking.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class FeedBack extends Component {
+  render() {
+    return (
+      <div>
+        <h3 data-testid="ranking-title">Ranking</h3>
+      </div>
+    );
+  }
+}
+
+export default FeedBack;
