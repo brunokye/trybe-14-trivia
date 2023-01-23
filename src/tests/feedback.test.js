70e45b181e3eceebb7af00219193a3b36e6fbaae
Guilherme Polkorny <gpolkorny@gmail.com>
2023-01-23 02:24:05 -0300

    mudei a logica, mas falta o resto

--- src/tests/feedback.test.js
+++ src/tests/feedback.test.js
@@ -1,42 +1,77 @@
-import React from 'react';
 import { screen } from '@testing-library/react';
-import { renderWithRouterAndRedux } from './helpers/renderWithRouterAndRedux';
+import React from 'react';
 import Feedback from '../pages/Feedback';
+import { renderWithRouterAndRedux } from './helpers/renderWithRouterAndRedux';
+import userEvent from '@testing-library/user-event';
 
 describe('When render <Feedback/>', () => {
-  it('Should contain a play again btn', () => {
-    renderWithRouterAndRedux(<Feedback />);
-    const playAgainBtn = screen.getByTestId('btn-play-again');
-    expect(playAgainBtn).toBeInTheDocument();
+  it('Should contain a play again btn whose return to the home', () => {
+    const { history } = renderWithRouterAndRedux(<Feedback />);
+    
+    expect(screen.getByTestId('btn-play-again')).toBeInTheDocument();
+  
+  
+    userEvent.click(screen.getByTestId('btn-play-again'));
+    
+    const { pathname } = history.location;
+  
+    expect(pathname).toBe('/');
   });
 
-  it('Should contain a ranking btn', () => {
-    renderWithRouterAndRedux(<Feedback />);
-    const rankingBtn = screen.getByTestId('btn-ranking');
-    expect(rankingBtn).toBeInTheDocument();
-  });
+it('Should contain a ranking btn whose redirect to ranking page', async () => {
+  const { history } = renderWithRouterAndRedux(<Feedback />);
 
-  it('Should contain a avatar img', () => {
-    renderWithRouterAndRedux(<Feedback />);
-    const avatarImg = screen.getByTestId('header-profile-picture');
-    expect(avatarImg).toBeInTheDocument();
-  });
+  expect(screen.getByRole('button', { name: /ranking/i })).toBeInTheDocument();
 
-  it('Should contain a name field', () => {
-    renderWithRouterAndRedux(<Feedback />);
-    const name = screen.getByTestId('header-player-name');
-    expect(name).toBeInTheDocument();
-  });
+  userEvent.click(screen.getByRole('button', { name: /ranking/i }));
 
-  it('Should contain a score field', () => {
+  const { pathname } = history.location;
+  
+  expect(pathname).toBe('/ranking');
+  
+});
+
+  it('Should contain a correct question counter', () => {
     renderWithRouterAndRedux(<Feedback />);
-    const score = screen.getByTestId('header-score');
-    expect(score).toBeInTheDocument();
+    
+    const questions = screen.getByTestId('feedback-total-question');
+    
+    expect(questions).toBeInTheDocument();
   });
 
   it('Should contain a text field', () => {
+    
     renderWithRouterAndRedux(<Feedback />);
+    
     const feedbackTxt = screen.getByTestId('feedback-text');
+    
     expect(feedbackTxt).toBeInTheDocument();
+  });
+
+  it('Should contain a avatar img', () => {
+    
+    renderWithRouterAndRedux(<Feedback />);
+    
+    const avatarImg = screen.getByTestId('header-profile-picture');
+    
+    expect(avatarImg).toBeInTheDocument();
+  });
+
+  it('Should contain a score field', () => {
+    
+    renderWithRouterAndRedux(<Feedback />);
+    
+    const score = screen.getByTestId('header-score');
+    
+    expect(score).toBeInTheDocument();
+  });
+
+  it('Should contain a name field', () => {
+    
+    renderWithRouterAndRedux(<Feedback />);
+    
+    const name = screen.getByTestId('header-player-name');
+    
+    expect(name).toBeInTheDocument();
   });
 });
